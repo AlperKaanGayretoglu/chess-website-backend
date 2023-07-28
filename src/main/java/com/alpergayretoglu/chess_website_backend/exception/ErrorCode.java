664dbf2ab900796ea170054435edbd1d9bb8040f
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-07-28 19:08:50 +0300

    [UPDATE] Made static ErrorCodes final

--- src/main/java/com/alpergayretoglu/chess_website_backend/exception/ErrorCode.java
+++ src/main/java/com/alpergayretoglu/chess_website_backend/exception/ErrorCode.java
@@ -5,19 +5,20 @@ import org.springframework.http.HttpStatus;
 
 @ToString
 public class ErrorCode {
-    public static ErrorCode UNKNOWN = new ErrorCode(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error");
-    public static ErrorCode VALIDATION = new ErrorCode(HttpStatus.UNPROCESSABLE_ENTITY, "Validation failed");
+    public static final ErrorCode UNKNOWN = new ErrorCode(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error");
+    public static final ErrorCode VALIDATION = new ErrorCode(HttpStatus.UNPROCESSABLE_ENTITY, "Validation failed");
 
-    public static ErrorCode AUTHENTICATED_USER_NOT_FOUND = new ErrorCode(HttpStatus.FORBIDDEN, "Authenticated user not found");
-    public static ErrorCode USER_IS_NOT_ADMIN = new ErrorCode(HttpStatus.FORBIDDEN, "User is not admin");
-    public static ErrorCode USER_IS_NOT_SELF = new ErrorCode(HttpStatus.FORBIDDEN, "User is not self");
-    public static ErrorCode USER_IS_NOT_ADMIN_OR_SELF = new ErrorCode(HttpStatus.FORBIDDEN, "User is not admin or self");
-    public static ErrorCode PASSWORD_MISMATCH = new ErrorCode(HttpStatus.FORBIDDEN, "Password mismatch");
+    public static final ErrorCode AUTHENTICATED_USER_NOT_FOUND = new ErrorCode(HttpStatus.FORBIDDEN, "Authenticated user not found");
+    public static final ErrorCode USER_IS_NOT_ADMIN = new ErrorCode(HttpStatus.FORBIDDEN, "User is not admin");
+    public static final ErrorCode USER_IS_NOT_SELF = new ErrorCode(HttpStatus.FORBIDDEN, "User is not self");
+    public static final ErrorCode USER_IS_NOT_ADMIN_OR_SELF = new ErrorCode(HttpStatus.FORBIDDEN, "User is not admin or self");
+    public static final ErrorCode PASSWORD_MISMATCH = new ErrorCode(HttpStatus.FORBIDDEN, "Password mismatch");
 
-    public static ErrorCode ACCOUNT_ALREADY_EXISTS = new ErrorCode(HttpStatus.CONFLICT, "Account already exists");
 
-    public static ErrorCode USER_NOT_FOUND = new ErrorCode(HttpStatus.NOT_FOUND, "User not found");
-    public static ErrorCode CHAT_NOT_FOUND = new ErrorCode(HttpStatus.NOT_FOUND, "Chat not found");
+    public static final ErrorCode ACCOUNT_ALREADY_EXISTS = new ErrorCode(HttpStatus.CONFLICT, "Account already exists");
+
+    public static final ErrorCode USER_NOT_FOUND = new ErrorCode(HttpStatus.NOT_FOUND, "User not found");
+    public static final ErrorCode CHAT_NOT_FOUND = new ErrorCode(HttpStatus.NOT_FOUND, "Chat not found");
 
     private final HttpStatus httpStatus;
     private final String message;
