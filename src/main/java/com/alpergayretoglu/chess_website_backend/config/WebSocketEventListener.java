0cd0fee9ff9b9d77a44c75e4cdf4e25d5e23ecf6
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-07-29 19:22:50 +0300

    [FEATURE] WebSocketEventListener - Note: Can be improved (for example, you could print out the username of the user that created the connection)

---
+++ src/main/java/com/alpergayretoglu/chess_website_backend/config/WebSocketEventListener.java
@@ -0,0 +1,25 @@
+package com.alpergayretoglu.chess_website_backend.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        LOGGER.info("Received a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        LOGGER.info("A web socket connection is disconnected");
+    }
+
+}
