7d47c79fbcb02905c658315af48b77267885adc2
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-09-02 14:24:29 +0300

    [CHANGE] Remove MovePattern from ChessPieceType and give it to ChessPieceLegalMoveService

--- src/main/java/com/alpergayretoglu/chess_website_backend/model/enums/ChessPieceType.java
+++ src/main/java/com/alpergayretoglu/chess_website_backend/model/enums/ChessPieceType.java
@@ -1,36 +1,20 @@
 package com.alpergayretoglu.chess_website_backend.model.enums;
 
-import com.alpergayretoglu.chess_website_backend.entity.chess.movePattern.MovePattern;
 import lombok.Getter;
 
 @Getter
 public enum ChessPieceType {
-    PAWN(1, new MovePattern(1, new int[][]{
-            {0, 1}
-    })),
-    KNIGHT(3, new MovePattern(1, new int[][]{
-            {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}
-    })),
-    BISHOP(3, new MovePattern(new int[][]{
-            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
-    })),
-    ROOK(5, new MovePattern(new int[][]{
-            {1, 0}, {0, 1}, {-1, 0}, {0, -1}
-    })),
-    QUEEN(9, new MovePattern(new int[][]{
-            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}
-    })),
-    KING(100, new MovePattern(1, new int[][]{
-            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}
-    }));
+    PAWN(1),
+    KNIGHT(3),
+    BISHOP(3),
+    ROOK(5),
+    QUEEN(9),
+    KING(100);
 
     private final int point;
 
-    private final MovePattern movePattern;
-
-    ChessPieceType(int point, MovePattern movePattern) {
+    ChessPieceType(int point) {
         this.point = point;
-        this.movePattern = movePattern;
     }
 
 
--- src/main/java/com/alpergayretoglu/chess_website_backend/service/ChessPieceLegalMoveService.java
+++ src/main/java/com/alpergayretoglu/chess_website_backend/service/ChessPieceLegalMoveService.java
@@ -1,35 +1,65 @@
 package com.alpergayretoglu.chess_website_backend.service;
 
-import com.alpergayretoglu.chess_website_backend.entity.chess.*;
+import com.alpergayretoglu.chess_website_backend.entity.chess.ChessCoordinate;
+import com.alpergayretoglu.chess_website_backend.entity.chess.ChessGame;
+import com.alpergayretoglu.chess_website_backend.entity.chess.ChessMove;
+import com.alpergayretoglu.chess_website_backend.entity.chess.PlayedPieceMove;
+import com.alpergayretoglu.chess_website_backend.entity.chess.movePattern.MovePattern;
 import com.alpergayretoglu.chess_website_backend.entity.chess.movePattern.MovePatternIterator;
 import com.alpergayretoglu.chess_website_backend.model.enums.ChessPiece;
 import com.alpergayretoglu.chess_website_backend.model.enums.ChessPieceType;
-import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
-@AllArgsConstructor
+@NoArgsConstructor
 public class ChessPieceLegalMoveService {
     @FunctionalInterface
     private interface ChessPieceLegalMoveCalculator {
-        List<ChessMove> calculateLegalMoves(ChessBoard chessBoard, ChessCoordinate currentCoordinate);
+        void calculateLegalMoves(
+                ChessGame chessGame,
+                ChessBoardPiecesObserver chessBoardPiecesObserver,
+                ChessCoordinate currentCoordinate,
+                List<ChessMove> legalMoves,
+                List<PlayedPieceMove> playedPieceMoves
+        );
     }
 
     private static final Map<ChessPieceType, ChessPieceLegalMoveCalculator> chessPieceLegalMoveCalculatorMap;
+    private static final Map<ChessPieceType, MovePattern> chessPieceBasicMovePatternMap;
 
     static {
         chessPieceLegalMoveCalculatorMap = new HashMap<>();
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.PAWN, (chessBoard, currentCoordinate) -> new ArrayList<>());
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.ROOK, (chessBoard, currentCoordinate) -> new ArrayList<>());
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.KNIGHT, (chessBoard, currentCoordinate) -> new ArrayList<>());
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.BISHOP, (chessBoard, currentCoordinate) -> new ArrayList<>());
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.QUEEN, (chessBoard, currentCoordinate) -> new ArrayList<>());
-        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.KING, (chessBoard, currentCoordinate) -> new ArrayList<>());
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.PAWN, ChessPieceLegalMoveService::calculateLegalMovesForPawn);
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.ROOK, ChessPieceLegalMoveService::calculateLegalMovesForRook);
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.KNIGHT, ChessPieceLegalMoveService::calculateLegalMovesForKnight);
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.BISHOP, ChessPieceLegalMoveService::calculateLegalMovesForBishop);
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.QUEEN, ChessPieceLegalMoveService::calculateLegalMovesForQueen);
+        chessPieceLegalMoveCalculatorMap.put(ChessPieceType.KING, ChessPieceLegalMoveService::calculateLegalMovesForKing);
+
+        chessPieceBasicMovePatternMap = new HashMap<>();
+        chessPieceBasicMovePatternMap.put(ChessPieceType.PAWN, new MovePattern(1, new int[][]{
+                {0, 1}
+        }));
+        chessPieceBasicMovePatternMap.put(ChessPieceType.KNIGHT, new MovePattern(1, new int[][]{
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}
+        }));
+        chessPieceBasicMovePatternMap.put(ChessPieceType.BISHOP, new MovePattern(new int[][]{
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        }));
+        chessPieceBasicMovePatternMap.put(ChessPieceType.ROOK, new MovePattern(new int[][]{
+                {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+        }));
+        chessPieceBasicMovePatternMap.put(ChessPieceType.QUEEN, new MovePattern(new int[][]{
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+        }));
+        chessPieceBasicMovePatternMap.put(ChessPieceType.KING, new MovePattern(1, new int[][]{
+                {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+        }));
     }
 
     public void calculateLegalMovesForPieceAtSquare(
@@ -40,8 +70,7 @@ public class ChessPieceLegalMoveService {
             List<PlayedPieceMove> playedPieceMoves
     ) {
         ChessPiece chessPiece = chessBoardPiecesObserver.getChessPieceAt(currentCoordinate);
-
-        MovePatternIterator movePatternIterator = chessPiece.getChessPieceType().getMovePattern().getIteratorStartingAt(currentCoordinate);
+        MovePatternIterator movePatternIterator = chessPieceBasicMovePatternMap.get(chessPiece.getChessPieceType()).getIteratorStartingAt(currentCoordinate);
 
         while (movePatternIterator.hasNext()) {
             ChessCoordinate nextCoordinate = movePatternIterator.next();
@@ -58,6 +87,69 @@ public class ChessPieceLegalMoveService {
                 playedPieceMoves.add(playedPieceMove);
             }
         }
+
+        chessPieceLegalMoveCalculatorMap.get(chessBoardPiecesObserver.getChessPieceAt(currentCoordinate).getChessPieceType())
+                .calculateLegalMoves(chessGame, chessBoardPiecesObserver, currentCoordinate, legalMoves, playedPieceMoves);
+    }
+
+    private static void calculateLegalMovesForPawn(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
+    }
+
+    private static void calculateLegalMovesForRook(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
+    }
+
+    private static void calculateLegalMovesForKnight(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
+    }
+
+    private static void calculateLegalMovesForBishop(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
+    }
+
+    private static void calculateLegalMovesForQueen(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
+    }
+
+    private static void calculateLegalMovesForKing(
+            ChessGame chessGame,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessCoordinate currentCoordinate,
+            List<ChessMove> legalMoves,
+            List<PlayedPieceMove> playedPieceMoves
+    ) {
+
     }
 
 }
