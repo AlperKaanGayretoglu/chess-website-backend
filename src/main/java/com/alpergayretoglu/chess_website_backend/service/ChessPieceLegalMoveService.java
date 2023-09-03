a843e7be03c561d2c9891dbdf81a3df2499816ca
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-09-03 13:36:56 +0300

    [FEATURE] Add capturing ability to pawn

--- src/main/java/com/alpergayretoglu/chess_website_backend/service/ChessPieceLegalMoveService.java
+++ src/main/java/com/alpergayretoglu/chess_website_backend/service/ChessPieceLegalMoveService.java
@@ -83,17 +83,26 @@ public class ChessPieceLegalMoveService {
         ChessColor pawnColor = chessBoardPiecesObserver.getChessPieceAt(currentCoordinate).getChessColor();
 
         boolean isPawnAtStartingPosition;
+
         int oneStepForwardRow;
         int twoStepsForwardRow;
+
+        int rightDiagonalColumn;
+        int leftDiagonalColumn;
+
 
         if (pawnColor == ChessColor.WHITE) {
             isPawnAtStartingPosition = currentCoordinate.getRow() == 6;
             oneStepForwardRow = currentCoordinate.getRow() - 1;
             twoStepsForwardRow = currentCoordinate.getRow() - 2;
+            rightDiagonalColumn = currentCoordinate.getColumn() + 1;
+            leftDiagonalColumn = currentCoordinate.getColumn() - 1;
         } else {
             isPawnAtStartingPosition = currentCoordinate.getRow() == 1;
             oneStepForwardRow = currentCoordinate.getRow() + 1;
             twoStepsForwardRow = currentCoordinate.getRow() + 2;
+            rightDiagonalColumn = currentCoordinate.getColumn() - 1;
+            leftDiagonalColumn = currentCoordinate.getColumn() + 1;
         }
 
         boolean isFirstMoveRegistered = registerPawnMove(
@@ -112,6 +121,22 @@ public class ChessPieceLegalMoveService {
                     ChessMoveType.PAWN_DOUBLE_MOVEMENT
             );
         }
+
+        registerPawnCapture(
+                currentCoordinate,
+                oneStepForwardRow,
+                rightDiagonalColumn,
+                chessBoardPiecesObserver,
+                chessMoveRegisterer
+        );
+
+        registerPawnCapture(
+                currentCoordinate,
+                oneStepForwardRow,
+                leftDiagonalColumn,
+                chessBoardPiecesObserver,
+                chessMoveRegisterer
+        );
 
     }
 
@@ -138,6 +163,26 @@ public class ChessPieceLegalMoveService {
                         .build()
         );
         return true;
+    }
+
+    private static void registerPawnCapture(
+            ChessCoordinate initialCoordinate,
+            int moveToRow,
+            int moveToColumn,
+            ChessBoardPiecesObserver chessBoardPiecesObserver,
+            ChessMoveRegisterer chessMoveRegisterer
+    ) {
+        if (!ChessCoordinate.isCoordinateValid(moveToRow, moveToColumn)) {
+            return;
+        }
+
+        ChessCoordinate toCoordinate = new ChessCoordinate(moveToRow, moveToColumn);
+        ChessPiece potentialEnemyPiece = chessBoardPiecesObserver.getChessPieceAt(toCoordinate);
+        if (potentialEnemyPiece == null || potentialEnemyPiece.getChessColor() == chessBoardPiecesObserver.getChessPieceAt(initialCoordinate).getChessColor()) {
+            return;
+        }
+
+        chessMoveRegisterer.registerNewNormalCaptureMove(initialCoordinate, toCoordinate);
     }
 
     private static void calculateLegalMovesForKing(
